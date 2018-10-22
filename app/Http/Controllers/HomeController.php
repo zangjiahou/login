aa70657e4a87874dd1764e77151275e6aefd11dd
adding table <zangjiahou@hotmail.com>
2018-10-22 17:56:01 -0400

    editting for error messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,8 +30,6 @@ class HomeController extends Controller
     }
     public function profile()
     {
-        $user = User::find(2);
-        $user->delete();
 
         $users = User::all();
         return view('profile', ['users' => $users]);
