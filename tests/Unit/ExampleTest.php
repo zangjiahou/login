97ce5db34301d41fe4f47262f3864d24bf4ba6ec
adding table <zangjiahou@hotmail.com>
2018-10-25 16:04:29 -0400

    register test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
 
 class ExampleTest extends TestCase
 {
@@ -14,6 +17,11 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $user = User::inRandomOrder()->first();
+        $this->assertInternalType('int',$user->id);
+        $this->assertInstanceOf('App\User',$user);
+
+        //$this->assertTrue(true);
     }
+
 }
